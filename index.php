040c3dae3b23559cb935bb5c9a98ea63967de818
Diego Gómez González <93382813+diegogomezcampusfp@users.noreply.github.com>
2022-01-21 14:07:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,43 +4,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Actividad_2022/01/21</title>
+    <title>funciones_resueltas</title>
 </head>
 <body>
-<?php
-//Función mayúscula
-echo ucfirst("<h2>hola soy diego</h2>");
-//Función reemplazar
-echo str_replace("a","e", "Hola" . "\n");
-//Función fecha
-setlocale(LC_ALL, "es_ES", 'Spanish_Spain', 'Spanish');
-$string = "21/01/2022";
-$date = DateTime::createFromFormat("d/m/Y", $string);
-echo strftime("%B",$date->getTimestamp());
-//Función muestras PHP y Server
-phpinfo();
-//Función número primo
-function esPrimo($n)
-{
- for($x=2; $x<$n; $x++)
-   {
-      if($n %$x ==0)
-	      {
-		   return 0;
-		  }
-    }
-  return 1;
-   }
-$a = esPrimo(3);
-if ($a==0)
-echo 'No es un número primo'."\n";
-else
-echo 'Es un número primo...'."\n";
-//Función año bisiesto
-$año = 2022;
-$bisiesto = date('L', mktime(0, 0, 0, 1, 1, $año));
-echo $año . ' ' . ($bisiesto ? 'es' : 'no es') . ' un año bisiesto.';
-?>
+    <h2>Actividad funciones PHP</h2>
+    <h3>Convertir texto con primera letra en mayúscula</h3>
+    <?php
+        require_once("libreria.php");
+        $resultadoMayuscula = primeraMayuscula("un texto");
+        echo $resultadoMayuscula;
+    ?>
 
+    <h3>Reemplazar la a por la e</h3>
+    <?php
+        require_once("libreria.php");
+        $resultadoReemplazar = reemplazar("a", "e");
+        echo $resultadoReemplazar;
+    ?>
+
+<h3>Mostrar mes</h3>
+    <?php
+        require_once("libreria.php");
+        $resultadoMes = mostrarMes("01/03/2022");
+        echo $resultadoMes;
+    ?>
+
+<h3>Mostrar info</h3>
+    <?php
+        require_once("libreria.php");
+        $resultadoInfo = Info("01/03/2022");
+        echo $resultadoInfo;
+    ?>
 </body>
 </html>
